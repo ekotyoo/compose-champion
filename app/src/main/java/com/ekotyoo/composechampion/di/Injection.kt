feaf7a412532377975ce3381190a3b1d0c926096
ekotyoo <ekeprasetyo@gmail.com>
2022-11-30 10:59:17 +0700

    add service locator

--- app/src/main/java/com/ekotyoo/composechampion/di/Injection.kt
+++ app/src/main/java/com/ekotyoo/composechampion/di/Injection.kt
@@ -1,4 +1,22 @@
 package com.ekotyoo.composechampion.di
 
+import com.ekotyoo.composechampion.data.fake.FakeMovieDataSource
+import com.ekotyoo.composechampion.data.repository.FakeMovieRepositoryImpl
+import com.ekotyoo.composechampion.domain.repository.MovieRepository
+import com.ekotyoo.composechampion.domain.usecase.AddMovieToFavoriteUseCase
+import com.ekotyoo.composechampion.domain.usecase.GetMovieDetailUseCase
+import com.ekotyoo.composechampion.domain.usecase.GetMoviesUseCase
+
 object Injection {
+    private fun provideDataSource(): FakeMovieDataSource = FakeMovieDataSource
+    private fun provideMovieRepository(): MovieRepository =
+        FakeMovieRepositoryImpl.getInstance(provideDataSource())
+
+    fun provideAddMoveToFavoriteUseCase(): AddMovieToFavoriteUseCase =
+        AddMovieToFavoriteUseCase(provideMovieRepository())
+
+    fun provideGetMovieDetailUseCase(): GetMovieDetailUseCase =
+        GetMovieDetailUseCase(provideMovieRepository())
+
+    fun provideGetMoviesUseCase(): GetMoviesUseCase = GetMoviesUseCase(provideMovieRepository())
 }
