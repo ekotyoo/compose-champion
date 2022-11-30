36607e03ab85b209ade2be9e0282ab64eea3fe15
ekotyoo <ekeprasetyo@gmail.com>
2022-11-30 11:00:27 +0700

    refactor GetMovieDetailUseCase

--- app/src/main/java/com/ekotyoo/composechampion/domain/usecase/GetMovieDetailUseCase.kt
+++ app/src/main/java/com/ekotyoo/composechampion/domain/usecase/GetMovieDetailUseCase.kt
@@ -4,6 +4,6 @@ import com.ekotyoo.composechampion.domain.model.MovieDetail
 import com.ekotyoo.composechampion.domain.repository.MovieRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetMovieDetail(private val movieRepository: MovieRepository) {
+class GetMovieDetailUseCase(private val movieRepository: MovieRepository) {
     fun invoke(movieId: String): Flow<MovieDetail> = movieRepository.getMovieDetail(movieId)
 }
